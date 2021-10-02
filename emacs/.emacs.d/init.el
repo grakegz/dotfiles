600864d0f431120202053559f8838511a9debb44
grakegz <grakegz@pm.me>
2021-10-02 22:42:20 +0200

    Configure orgmode level sizes and title section.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -22,6 +22,7 @@
 (set-fringe-mode 10)
 (size-indication-mode t)          ;; show size of current file in modeline
 (setq visible-bell t)             ;; stop that beeping
+(delete-selection-mode t)         ;; overwrite selected text
 
 ;; Start emacs in fullscreen mode
 ;; (custom-set-variables
@@ -175,3 +176,30 @@
 (global-set-key (kbd "<M-s-down>") 'enlarge-window)
 (global-set-key (kbd "<M-s-left>") 'shrink-window-horizontally)
 (global-set-key (kbd "<M-s-right>") 'enlarge-window-horizontally)
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:inherit outline-8 :extend nil :height 1.5))))
+ '(org-document-info ((t (:inherit org-document-info :extend nil :height 1.2))))
+ '(org-document-info-keyword ((t (:inherit org-document-info-keyword :extend nil :height 1.2))))
+ '(org-level-1 ((t (:inherit outline-1 :extend nil :height 1.5))))
+ '(org-level-2 ((t (:inherit outline-2 :extend nil :height 1.4))))
+ '(org-level-3 ((t (:inherit outline-3 :extend nil :height 1.3))))
+ '(org-level-4 ((t (:inherit outline-4 :extend nil :height 1.2))))
+ '(org-level-5 ((t (:inherit outline-5 :extend nil :height 1.1))))
+ '(org-level-6 ((t (:inherit outline-6 :extend nil :height 1.0))))
+ '(org-level-7 ((t (:inherit outline-7 :extend nil :height 1.0))))
+ '(org-level-8 ((t (:inherit outline-8 :extend nil :height 1.0))))
+)
